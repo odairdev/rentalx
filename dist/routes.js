117506194364407df8fac369a63f018b29971446
Odair Jose Custodio Junior <odair.dev@gmail.com>
2022-05-17 19:18:04 -0300

    Delete dist directory

--- dist/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-exports.routes = (0, express_1.Router)();
-exports.routes.get('/', (request, response) => {
-    response.send('hello world');
-});
