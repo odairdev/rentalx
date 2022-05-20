74307570dc4e0d6d8ea3176609142519ef1bc254
Odair José Custodio Junior <odair.dev@gmail.com>
2022-05-20 16:40:46 -0300

    Swagger documentation implemented

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import { router } from './routes/index';
 import express from 'express'
+import swaggerUI from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
 
 const server = express()
 const port = 3333
 
 server.use(express.json())
 
+server.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile))
+
 server.use(router)
+
+
 
 server.listen(port, () => {
   console.log('Server running on port ' + port)
